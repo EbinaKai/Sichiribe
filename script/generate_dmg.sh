1f58c823694892bb835233cef20e356c46a06b06
EbinaKai <blackbicycle55@gmail.com>
2024-09-12 19:19:19 +0300

    [remove] script/generate_dmg.sh

--- script/generate_dmg.sh
+++
@@ -1,8 +0,0 @@
-# /usr/local/bin/
-
-create-dmg --window-size 920 660 \
-           --background "background.png" \
-           --icon "Sichiribe.app" 200 240 \
-           --app-drop-link 550 240 \
-           Sichiribe.dmg \
-           Sichiribe.app
